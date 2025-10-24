9b8649fee087bf1a1f0c29652b93a0ef2737b058
Kantin FAGNIART <140246837+KANTIN-FAGN@users.noreply.github.com>
2025-10-24 11:48:16 +0200

    feat (rate limiter): add rate limiter on all routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
-import { ArticlesModule } from './articles/articles.module';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import {Module} from '@nestjs/common';
+import {PrismaModule} from './prisma/prisma.module';
+import {ArticlesModule} from './articles/articles.module';
+import {AuthModule} from './auth/auth.module';
+import {UsersModule} from './users/users.module';
+import {ThrottlerModule, ThrottlerGuard} from '@nestjs/throttler';
+import {APP_GUARD} from '@nestjs/core';
 
 @Module({
-  imports: [PrismaModule, AuthModule, ArticlesModule, UsersModule],
+    imports: [
+        PrismaModule,
+        AuthModule,
+        ArticlesModule,
+        UsersModule,
+        ThrottlerModule.forRoot([
+            { name: 'short', ttl: 10000, limit: 5 },
+        ]),
+    ],
+    providers: [
+        { provide: APP_GUARD, useClass: ThrottlerGuard },
+    ],
 })
 export class AppModule {}
--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -9,22 +9,24 @@ import {
     ParseIntPipe, UseGuards,
     Query,
 } from '@nestjs/common';
-import { ArticlesService } from './articles.service';
-import { CreateArticleDto } from './dto/create-article.dto';
-import { UpdateArticleDto } from './dto/update-article.dto';
-import { ApiBearerAuth, ApiCreatedResponse, ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
-import { ArticleEntity } from './entities/article.entity';
-import { JwtAuthGuard } from "../auth/jwt-auth.guard";
+import {ArticlesService} from './articles.service';
+import {CreateArticleDto} from './dto/create-article.dto';
+import {UpdateArticleDto} from './dto/update-article.dto';
+import {ApiBearerAuth, ApiCreatedResponse, ApiOkResponse, ApiQuery, ApiTags} from '@nestjs/swagger';
+import {ArticleEntity} from './entities/article.entity';
+import {JwtAuthGuard} from "../auth/jwt-auth.guard";
+import {SkipThrottle, Throttle} from '@nestjs/throttler';
 
 @Controller('articles')
 @ApiTags('articles')
 export class ArticlesController {
-    constructor(private readonly articlesService: ArticlesService) { }
+    constructor(private readonly articlesService: ArticlesService) {
+    }
 
     @Post()
     @UseGuards(JwtAuthGuard)
     @ApiBearerAuth()
-    @ApiCreatedResponse({ type: ArticleEntity })
+    @ApiCreatedResponse({type: ArticleEntity})
     async create(@Body() createArticleDto: CreateArticleDto) {
         return new ArticleEntity(
             await this.articlesService.create(createArticleDto),
@@ -35,14 +37,14 @@ export class ArticlesController {
     //pagination ici :
 
     @Get()
-    @ApiOkResponse({ type: ArticleEntity, isArray: true })
-    @ApiQuery({ name: 'page', required: false, type: Number, example: 1 })
-    @ApiQuery({ name: 'limit', required: false, type: Number, example: 10 })
+    @ApiOkResponse({type: ArticleEntity, isArray: true})
+    @ApiQuery({name: 'page', required: false, type: Number, example: 1})
+    @ApiQuery({name: 'limit', required: false, type: Number, example: 10})
     async findAll(
         @Query('page') page: number = 1,
         @Query('limit') limit: number = 10,
     ) {
-        const { data, total } = await this.articlesService.findAll(page, limit);
+        const {data, total} = await this.articlesService.findAll(page, limit);
         return {
             articles: data.map((article) => new ArticleEntity(article)),
             meta: {
@@ -58,14 +60,14 @@ export class ArticlesController {
     @Get('drafts')
     @UseGuards(JwtAuthGuard)
     @ApiBearerAuth()
-    @ApiOkResponse({ type: ArticleEntity, isArray: true })
-    @ApiQuery({ name: 'page', required: false, type: Number, example: 1 })
-    @ApiQuery({ name: 'limit', required: false, type: Number, example: 10 })
+    @ApiOkResponse({type: ArticleEntity, isArray: true})
+    @ApiQuery({name: 'page', required: false, type: Number, example: 1})
+    @ApiQuery({name: 'limit', required: false, type: Number, example: 10})
     async findDrafts(
         @Query('page') page: number = 1,
         @Query('limit') limit: number = 10,
     ) {
-        const { data, total } = await this.articlesService.findDrafts(page, limit);
+        const {data, total} = await this.articlesService.findDrafts(page, limit);
         return {
             data: data.map((draft) => new ArticleEntity(draft)),
             meta: {
@@ -79,7 +81,7 @@ export class ArticlesController {
 
 
     @Get(':id')
-    @ApiOkResponse({ type: ArticleEntity })
+    @ApiOkResponse({type: ArticleEntity})
     async findOne(@Param('id', ParseIntPipe) id: number) {
         return new ArticleEntity(await this.articlesService.findOne(id));
     }
@@ -87,7 +89,7 @@ export class ArticlesController {
     @Patch(':id')
     @UseGuards(JwtAuthGuard)
     @ApiBearerAuth()
-    @ApiCreatedResponse({ type: ArticleEntity })
+    @ApiCreatedResponse({type: ArticleEntity})
     async update(
         @Param('id', ParseIntPipe) id: number,
         @Body() updateArticleDto: UpdateArticleDto,
@@ -100,7 +102,7 @@ export class ArticlesController {
     @Delete(':id')
     @UseGuards(JwtAuthGuard)
     @ApiBearerAuth()
-    @ApiOkResponse({ type: ArticleEntity })
+    @ApiOkResponse({type: ArticleEntity})
     async remove(@Param('id', ParseIntPipe) id: number) {
         return new ArticleEntity(await this.articlesService.remove(id));
     }
